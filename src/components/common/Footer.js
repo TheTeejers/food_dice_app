886f1fb64ac7c18ce40b5375316625e6d899fb43
TJ Loughry <tjloughry@gmail.com>
2017-11-06 13:59:07 -0600

    footer added

---
+++ src/components/common/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer>
+        <p className='pfooter'>Made by Jay Lara, TJ Loughry, Fransisco Ordaz, and Carrington Simecheck at General Assembly</p>
+      </footer>
+    )
+  }
+}
+
+export default Footer;
